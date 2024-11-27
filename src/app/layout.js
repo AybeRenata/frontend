d65638a8839ab97e6424d626e0112b874182ef37
AybeRenata <ayberenatalucia12@gmail.com>
2024-11-27 18:12:24 -0300

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Separator } from "@/components/ui/separator";
+import { SidebarNav } from "@/components/ui/sidebar-nav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,12 +20,35 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const sidebarNavItems = [
+    {
+      title: "Productos",
+      href: "/products",
+    },
+
+  ]
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="hidden space-y-6 p-10 pb-16 md:block">
+
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">Administrador</h2>
+
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="-mx-4 lg:w-1/5">
+              <SidebarNav items={sidebarNavItems} />
+            </aside>
+            <div className="flex-1 lg:max-w-2xl">{children}</div>
+          </div>
+        </div>
+
       </body>
     </html>
   );
